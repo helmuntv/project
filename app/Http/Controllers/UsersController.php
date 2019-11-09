857ebc27a2523d2bb06c88fd904e9464e6a4c9ca
helmuntv <hvelasqueze@ucentral.edu.co>
2019-11-09 16:23:20 -0500

    first commit

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+
+class UsersController extends Controller
+{
+    function index(Request $request){
+
+        if($request->isJson()){
+            $users = User::all();
+            return response()->json([$users], 200);
+        }
+        return response()->json(['error' => 'Unauthorized'], 401);
+    }
+
+    function createUser(Request $request){
+
+        if($request->isJson()){
+            $data = $request->json()->all();
+
+            $user = User::create([
+                'first_name' => $data['first_name'],
+                'last_name' => $data['last_name'],
+                'email' => $data['email'],
+                'password' => Hash::make($data['password']),
+                'token' => str_random(32)
+            ]);
+            return response()->json($user, 201);
+        }
+        return response()->json(['error' => 'Unauthorized'], 401);
+    }
+
+    function updateUser(Request $request, $id)
+    {
+        if ($request->isJson()) {
+            try {
+                $user = User::findOrFail($id);
+                $data = $request->json()->all();
+                $user->first_name = $data['first_name'];
+                $user->last_name = $data['last_name'];
+                $user->email = $data['email'];
+                $user->password = Hash::make($data['password']);
+                $user->save();
+                return response()->json($user, 204);
+            } catch (ModelNotFoundException $e) {
+                return response()->json(['error' => 'No content'], 406);
+            }
+        } else {
+            return response()->json(['error' => 'Unauthorized'], 401, []);
+        }
+    }
+
+    function deleteUser(Request $request, $id)
+    {
+        if ($request->isJson()) {
+            try {
+                $user = User::findOrFail($id);
+                $user->delete();
+                return response()->json($user, 200);
+            } catch (ModelNotFoundException $e) {
+                return response()->json(['error' => 'No content'], 406);
+            }
+        } else {
+            return response()->json(['error' => 'Unauthorized'], 401, []);
+        }
+    }
+
+    //login
+    function getToken(Request $request){
+        if($request->isJson()){
+            try{
+                $data = $request->json()->all();
+
+                $user = User::where('email', $data['email'])->first();
+                if($user && Hash::check($data['password'], $user->password)){
+                    return response()->json($user, 200);
+                }else{
+                    return response()->json(['error' => 'Error in user or password'], 401);
+                }
+            }catch(ModelNotFoundException $e){
+                return response()->json(['error' => 'Error in user or password'], 401);
+            }
+        }
+        return response()->json(['error' => 'Unauthorized'], 401);
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+
+$router->post('/login',['uses' => 'UsersController@getToken']);
+
+$router->get('/', function () use ($router) {
+    return $router->app->version();
+});
+
+$router->get('/key', function(){
+    return str_random(32);
+});
+
+$router->group(['middleware' => ['auth']], function() use ($router){
+    $router->get('/users', ['uses' => 'UsersController@index']);
+    $router->post('/users', ['uses' => 'UsersController@createUser']);
+    $router->put('/users/{id}', ['uses' => 'UsersController@updateUser']);
+    $router->delete('/users/{id}', ['uses' => 'UsersController@deleteUser']);
+});
+
+
+
